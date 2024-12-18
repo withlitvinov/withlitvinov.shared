147f1e6269bea9c60150a197d4808579c927872b
Anatolii Litvinov <anatolii.litvinov.r@gmail.com>
2024-12-18 03:50:09 +0200

    fix: update import

--- packages/eslint-config-react/src/index.ts
+++ packages/eslint-config-react/src/index.ts
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import _reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
+
+const reactRefresh = _reactRefresh.default;
 
 export default tseslint.config(
   { ignores: ["build", "eslint.config.js"] },
