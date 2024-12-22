149cc95bb641255451fc72470cf38cbb0d9cdeca
Anatolii Litvinov <anatolii.litvinov.r@gmail.com>
2024-12-22 13:52:52 +0200

    fix: check command execution for failed status
    * add `test` command

--- packages/eslint-config-react/test/index.test.ts
+++ packages/eslint-config-react/test/index.test.ts
@@ -10,8 +10,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 function runLintAgainst(projectName: string) {
-  const parentDir = path.join(__dirname, "../examples");
+  const parentDir = path.join(__dirname, "../../../examples");
   const projectDir = path.join(parentDir, projectName);
+
   // Use `pnpm` to avoid locating each `eslint` bin ourselves.
   // Use `--silent` to only print the output of the command, stripping the pnpm log.
   return execa({
@@ -25,7 +26,9 @@ describe("should pass lint without error in new project", () => {
   const projectName = "minimal";
 
   test(projectName, async () => {
-    const { stdout } = await runLintAgainst(projectName);
+    const { stdout, failed } = await runLintAgainst(projectName);
+
     expect(stdout).toMatch(WHITESPACE_ONLY);
+    expect(failed).toEqual(false);
   });
 });
