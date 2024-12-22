8cf3ab06d5c67955856cf4ea403f3eb2c5413c78
Anatolii Litvinov <anatolii.litvinov.r@gmail.com>
2024-12-22 02:43:01 +0200

    feat: add import sort

--- packages/eslint-config-react/src/index.ts
+++ packages/eslint-config-react/src/index.ts
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import _reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
+import eslintImport from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const reactRefresh = _reactRefresh.default;
 
@@ -17,6 +19,7 @@ export default tseslint.config(
         flat: true,
         semi: true,
       }),
+      eslintImport.flatConfigs.recommended,
     ],
     files: ["**/*.{js,jsx,ts,tsx,mjs}"],
     languageOptions: {
@@ -26,12 +29,56 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "unused-imports": unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
+      ],
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
+      "import/no-named-as-default-member": ["off"],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            orderImportKind: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/export": ["warn"],
+      "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+      "import/newline-after-import": "error",
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
       ],
     },
   },
